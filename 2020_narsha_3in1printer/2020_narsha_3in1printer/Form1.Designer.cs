0998e35b184b0d93f50817f9b2ecb016d8740cff
Leejimin <outum1500@gmail.com>
2020-08-01 09:21:35 +0900

    window_camera
    asdf

---
+++ 2020_narsha_3in1printer/2020_narsha_3in1printer/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace _2020_narsha_3in1printer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "↖";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "↑";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(124, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "↗";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button3_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "←";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(68, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ㅇ";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(124, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "→";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button6_MouseDown);
+            this.button6.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button6_MouseUp);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 124);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "↙";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button7_MouseDown);
+            this.button7.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button7_MouseUp);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(68, 124);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "↓";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button8_MouseDown);
+            this.button8.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button8_MouseUp);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(124, 124);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "↘";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button9_MouseDown);
+            this.button9.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button9_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ 2020_narsha_3in1printer/2020_narsha_3in1printer/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace _2020_narsha_3in1printer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%207%20-data1%2063%20-data2%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%201%20-data2%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%205%20-data1%2063%20-data2%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button3_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%203%20-data1%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button4_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button6_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%204%20-data1%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button6_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button7_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%208%20-data1%2063%20-data2%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button7_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button8_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%202%20-data2%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button8_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button9_MouseDown(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%206%20-data1%2063%20-data2%2063";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+
+        private void button9_MouseUp(object sender, MouseEventArgs e)
+        {
+            string TargetURL = "http://192.168.137.191/?jcpcmd=pelcod20ctrl%20-type%201%20-cmd%209";
+            HttpWebRequest gomRequest = (HttpWebRequest)WebRequest.Create(TargetURL);
+            gomRequest.Method = "GET";
+
+            WebResponse response = gomRequest.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+        }
+    }
+}
